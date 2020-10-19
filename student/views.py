6f1121301b5745c7c15d003cbe065d39d49b1b05
Laura Miller <lmiller165@gmail.com>
2020-10-19 16:32:44 -0600

    Student dashboard providing latest grade added for each course

--- student/views.py
+++ student/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from main.models import Grade, Gpa
+from main.models import Grade, Gpa, Course
 from main.api import charts
 from student.services import get_first_name
+import datetime
 
 
 
@@ -18,9 +19,17 @@ def dashboard(request):
     """
     gpas = Gpa.objects.filter(student=request.user).all()
     gpa_chart_json = charts.get_gpa_chart_data(gpas)
+    latest_class_avgs = {}
+
+    courses = Course.objects.filter(student=request.user.student).all()
+
+    for course in courses:
+        grade = Grade.objects.filter(course=course).latest('timestamp')
+        latest_class_avgs[course.title] = grade.grade
 
     context = {
-        'gpa_chart_json': gpa_chart_json
+        'gpa_chart_json': gpa_chart_json,
+        'latest_class_avgs': latest_class_avgs
     }
 
     return render(request, 'student/dashboard.html', context)
